b3969dfc0a45aaab806c29bb6f4a52db7d4481aa
savorgnan5 <savorgnan5@aol.com>
2018-12-14 16:26:44 -0500

    sd

--- PanAss.py
+++ PanAss.py
@@ -463,26 +463,30 @@ type_pd.head()
 group_type= type_pd.groupby(["school_type"])
 group_type.mean()
 
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+Interpretation of the data:
+    
+1-The district type of school has lowers math score, reading score, average math passing score, average reading passing score and average overall passing score.
+2-The large school has lowers math score, reading score, average math passing score, average reading passing score and average overall passing score.
+3-The school with highest budget per students has lowers math score, reading score average passing math score, average reading passing score and overall passing score.
 
 
 
 
 
-# In[ ]:
 
 
 
 
 
-# In[ ]:
+
+
+
+
+
+
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Pandas-Ass
+
+Interpretation of the data:
+    
+1-The district type of school has lowers math score, reading score, average math passing score, average reading passing score and average overall passing score.
+2-The large school has lowers math score, reading score, average math passing score, average reading passing score and average overall passing score.
+3-The school with highest budget per students has lowers math score, reading score average passing math score, average reading passing score and overall passing score.
